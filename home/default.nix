a3d6be0bf2c23bd9758bf013e3319496f88563f0
dxvampi <dxvampi@proton.me>
2026-09-25 00:03:37 +0200

    Add default home

---
+++ home/default.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hyprland.nix
+    ./kitty.nix
+    ./rofi.nix
+    ./swayosd.nix
+    ./waybar.nix
+  ];
+
+  _module.args.dotfiles = "/home/dxvampi/nixos-config/dotfiles";
+
+  home.stateVersion = "26.05";
+}
