455c33701173ce5b14a54e3fae566dd584e6f41f
dxvampi <dxvampi@proton.me>
2026-09-25 00:10:31 +0200

    Add backup to flake hm

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.dxvampi = import ./home;
+            home-manager.backupFileExtension = "backup";
           }
         ];
       };
